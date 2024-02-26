b838bd97590eb9373ced4aafce844b4a8c5a9b45
한상선 <hs94122@naver.com>
2024-02-26 22:10:01 +0900

    Initial Commit

---
+++ WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by 한상선 on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
