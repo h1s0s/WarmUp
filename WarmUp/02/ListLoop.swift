89ef30a329148e71bee1ed9d61e7a41fafea2112
한상선 <hs94122@naver.com>
2024-03-03 21:23:19 +0900

    2024-03-03 데이터모델링 배우기

--- WarmUp/02/ListLoop.swift
+++ WarmUp/02/ListLoop.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct ListLoop: View {
+    var favoriteFruits = [
+        Fruit(name: "Apple", matchFruitName: "Banana", price: 1000),
+        Fruit(name: "Banana", matchFruitName: "Banana", price: 3000),
+        Fruit(name: "Cherry", matchFruitName: "Durian", price: 4000),
+        Fruit(name: "Durian", matchFruitName: "ElderBery", price: 8000),
+    ]
     var fruits = ["Apple", "Banana", "Cherry", "Durian", "Elder Berry"]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(fruits, id: \.self) { fruit in
-                    Text(fruit)
+                ForEach(favoriteFruits, id: \.self) { fruit in
+                    HStack {
+                        Text(fruit.name)
+                        Text(fruit.matchFruitName)
+                        Text("\(fruit.price)")
+                    }
                 }
             }
             .navigationTitle("Fruist List")
@@ -22,6 +32,11 @@ struct ListLoop: View {
     }
 }
 
+struct Fruit: Hashable { //Hashable 반복될 수 있는
+    let name: String
+    let matchFruitName: String
+    let price: Int
+}
 #Preview {
     ListLoop()
 }
